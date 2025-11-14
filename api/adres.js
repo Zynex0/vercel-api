36003ce392268ce0478b43ddd3d7c62f5b6b76dc
ZYNEX00 <mertcankay29@gmail.com>
2025-11-15 01:50:48 +0300

    Create adres.js

---
+++ api/adres.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { tc } = req.query;
+
+  if (!tc) {
+    return res.status(400).json({ hata: "tc parametresi eksik" });
+  }
+
+  try {
+    const url = https://dosya.alwaysdata.net/api/adres.php?tc=${tc};
+    const r = await axios.get(url);
+
+    res.status(200).json(r.data);
+  } catch (err) {
+    res.status(500).json({ hata: "API çağrısı başarısız" });
+  }
+}
