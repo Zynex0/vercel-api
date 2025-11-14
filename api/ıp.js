8f0ba62c6b3b3c59b97d0dbed5d3a1ce49f75150
ZYNEX00 <mertcankay29@gmail.com>
2025-11-15 02:21:12 +0300

    Add API handler for 'ıp' query parameter

---
+++ api/ıp.js
@@ -0,0 +1,26 @@
+
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { ıp } = req.query;
+
+  if (!tc) {
+    return res.status(400).json({ hata: "tc parametresi eksik" });
+  }
+
+  try {
+    const url = `https://dosya.alwaysdata.net/api/ıp.php?tc=${ıp}`;
+    const r = await axios.get(url);
+
+    // Gelen veriyi kopyala
+    const data = { ...r.data };
+
+    // developer bölümünü değiştir
+    data.developer = "Zynex00";
+
+    return res.status(200).json(data);
+
+  } catch (err) {
+    return res.status(500).json({ hata: "API çağrısı başarısız" });
+  }
+}
