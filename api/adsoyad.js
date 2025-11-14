42c4f2e5adc918e4f0577e11c22a7d25d8fed6e7
ZYNEX00 <mertcankay29@gmail.com>
2025-11-15 02:24:11 +0300

    Create adsoyad.js

---
+++ api/adsoyad.js
@@ -0,0 +1,27 @@
+
+
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { ad, soyad } = req.query;
+
+  if (!tc) {
+    return res.status(400).json({ hata: "Ad Soyad Bilgileri Gereklidir" });
+  }
+
+  try {
+    const url = `https://dosya.alwaysdata.net/api/adsoyad.php?ad=${ad}&soyad=${soyad}`;
+    const r = await axios.get(url);
+
+    // Gelen veriyi kopyala
+    const data = { ...r.data };
+
+    // developer bölümünü değiştir
+    data.developer = "Zynex00";
+
+    return res.status(200).json(data);
+
+  } catch (err) {
+    return res.status(500).json({ hata: "API çağrısı başarısız" });
+  }
+}
