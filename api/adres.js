d2281648884fb9171d612daf512dd4f6b6f24eeb
ZYNEX00 <mertcankay29@gmail.com>
2025-11-15 02:11:36 +0300

    Update adres.js

--- api/adres.js
+++ api/adres.js
@@ -3,27 +3,23 @@ import axios from "axios";
 export default async function handler(req, res) {
   const { tc } = req.query;
 
-  // TC parametresi yoksa hata döndür
   if (!tc) {
     return res.status(400).json({ hata: "tc parametresi eksik" });
   }
 
   try {
-    // Uzak API isteği
     const url = `https://dosya.alwaysdata.net/api/adres.php?tc=${tc}`;
     const r = await axios.get(url);
 
     // Gelen veriyi kopyala
     const data = { ...r.data };
 
-    // "developer" alanını kaldır
-    delete data.developer;
+    // developer bölümünü değiştir
+    data.developer = "Zynex00";
 
-    // Sonucu geri gönder
     return res.status(200).json(data);
 
   } catch (err) {
-    // Hata durumunda
     return res.status(500).json({ hata: "API çağrısı başarısız" });
   }
 }
